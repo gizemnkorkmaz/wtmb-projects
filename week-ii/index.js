6c9da5a5046f21f02217d43df5060660893a7acb
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2021-05-22 20:02:12 +0300

    Restart the project

--- week-ii/index.js
+++
@@ -1,4 +0,0 @@
-const Database = require('./database')
-
-const loadedFile = Database.load('bookshelf.json')
-console.log(loadedFile.owner)
--- week-iii/index.js
+++
@@ -1,6 +0,0 @@
-const Bookshelf = require('./bookshelf')
-const Book = require('./book')
-const Database = require('./database')
-
-const loadedFile = Database.load('bookshelf.json')
-console.log(loadedFile.owner)
